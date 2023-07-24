281975b2748294d224ca7b5a4fb93ec4c7219561
unknown <meti.tx9@gmail.com>
2023-07-25 00:51:27 +0330

    Add navbar to page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  faCodepen,
+  faDraft2digital,
+  faGithub,
+  faLinkedin,
+  faTelegram,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faContactBook,
+  faHome,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { color, motion } from "framer-motion";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar />
+    // <div className="container">
+    //   <button className="btn">Hi</button>
+    //   <br />
+    //   <input type="text" placeholder="name" />
+    // </div>
+  );
+}
+function Navbar() {
+  return (
+    <div className="navbar">
+      <h1 className="logo">
+        <a href="/">Tx9</a>
+      </h1>
+      <nav className="nav-menu">
+        <motion.a href="/" whileHover={{ animation: "menuIcon 1s" }}>
+          <FontAwesomeIcon icon={faHome} className="icon"></FontAwesomeIcon>
+        </motion.a>
+        <motion.a href="/" whileHover={{ animation: "menuIcon 1s" }}>
+          <FontAwesomeIcon icon={faUser} className="icon"></FontAwesomeIcon>
+        </motion.a>
+        <motion.a href="/" whileHover={{ animation: "menuIcon 1s" }}>
+          <FontAwesomeIcon
+            icon={faDraft2digital}
+            className="icon"
+          ></FontAwesomeIcon>
+        </motion.a>
+        <motion.a href="/" whileHover={{ animation: "menuIcon 1s" }}>
+          <FontAwesomeIcon
+            icon={faContactBook}
+            className="icon"
+          ></FontAwesomeIcon>
+        </motion.a>
+      </nav>
+      <ul className="social-media">
+        <li>
+          <motion.a
+            href="/"
+            whileHover={{ color: "#0c0c", animation: "socialIcon 1s" }}
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="icon github"
+            ></FontAwesomeIcon>
+          </motion.a>
+        </li>
+        <li>
+          <motion.a
+            href="/"
+            whileHover={{
+              color: "#0c0c",
+              animation: "socialIcon 1s",
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="icon linkedin"
+            ></FontAwesomeIcon>
+          </motion.a>
+        </li>
+        <li>
+          <motion.a
+            href="/"
+            whileHover={{ color: "#0c0c", animation: "socialIcon 1s" }}
+          >
+            <FontAwesomeIcon
+              icon={faCodepen}
+              className="icon codepen"
+            ></FontAwesomeIcon>
+          </motion.a>
+        </li>
+        <li>
+          <motion.a
+            href="/"
+            whileHover={{ color: "#0c0c", animation: "socialIcon 1s" }}
+          >
+            <FontAwesomeIcon
+              icon={faTelegram}
+              className="icon telegram"
+            ></FontAwesomeIcon>
+          </motion.a>
+        </li>
+      </ul>
     </div>
   );
 }
